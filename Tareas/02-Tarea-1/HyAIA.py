710962d496c5487ad6ed5a729ac12cb7a16db78f
Miguel Cuevas <cuevasfm@gmail.com>
2025-11-22 12:46:03 -0600

    tarea 1 y tarea 2

--- Tareas/02-Tarea-1/HyAIA.py
+++ Tareas/02-Tarea-1/HyAIA.py
@@ -11,8 +11,6 @@ class HyAIA:
         self.data_categoricos, self.categoricos_columns = self.get_categoricos()
         self.df_dqr = self.get_dqr()
         
-    ##% Métodos para Análisis de Datos 
-    #Método para obtener las columnas y dataframe binarios
     def get_binarios(self):
         col_bin = []
         for col in self.data.columns:
@@ -20,12 +18,10 @@ class HyAIA:
                 col_bin.append(col)
         return self.data[col_bin], col_bin
         
-    #Método para obtener columnas y dataframe cuantitativos
     def get_cuantitativos(self):
         col_cuantitativas = self.data.select_dtypes(include='number').columns
         return self.data[col_cuantitativas], col_cuantitativas
         
-    #Método para obtener columnas y dataframe categóricos
     def get_categoricos(self):
         col_categoricos = self.data.select_dtypes(exclude='number').columns
         col_cat = []
@@ -35,38 +31,28 @@ class HyAIA:
         return self.data[col_cat], col_cat
         
     def get_dqr(self):
-        #% Lista de variables de la base de datos
         columns = pd.DataFrame(list(self.data.columns.values), columns=['Columns_Names'], 
                                index=list(self.data.columns.values))
         
-        #Lista de tipos de datos del dataframe
         data_dtypes = pd.DataFrame(self.data.dtypes, columns=['Dtypes'])
         
-        #Lista de valores presentes
         present_values = pd.DataFrame(self.data.count(), columns=['Present_values'])
         
-        #Lista de valores missing (Valores faltantes/nulos nan)
         missing_values = pd.DataFrame(self.data.isnull().sum(), columns=['Missing_values'])
         
-        #Valores unicos de las columnas
         unique_values = pd.DataFrame(columns=['Unique_values'])
         for col in list(self.data.columns.values):
             unique_values.loc[col] = [self.data[col].nunique()]
         
-        # TAREA 1: Columna booleana para identificar si es categórica
         is_categorical = pd.DataFrame(columns=['Is_categorical'])
         for col in list(self.data.columns.values):
-            # Si el tipo de dato no es numérico, es categórica
             is_categorical.loc[col] = [not pd.api.types.is_numeric_dtype(self.data[col])]
         
-        # TAREA 2: Columna con las categorías (si tiene <= 10 valores únicos)
         categories = pd.DataFrame(columns=['Categories'])
         for col in list(self.data.columns.values):
-            # Solo para columnas categóricas
             if not pd.api.types.is_numeric_dtype(self.data[col]):
                 n_unique = self.data[col].nunique()
                 if n_unique <= 10:
-                    # Obtener las categorías únicas como lista
                     cats = self.data[col].unique().tolist()
                     categories.loc[col] = [cats]
                 else:
@@ -74,8 +60,6 @@ class HyAIA:
             else:
                 categories.loc[col] = ['N/A']
         
-        # TAREA 3: Información estadística SOLO para columnas numéricas
-        #Lista de valores máximos
         max_values = pd.DataFrame(columns=['Max_values'])
         for col in list(self.data.columns.values):
             if pd.api.types.is_numeric_dtype(self.data[col]):
@@ -86,7 +70,6 @@ class HyAIA:
             else:
                 max_values.loc[col] = ['N/A']
         
-        #Lista de valores mínimos
         min_values = pd.DataFrame(columns=['Min_values'])
         for col in list(self.data.columns.values):
             if pd.api.types.is_numeric_dtype(self.data[col]):
@@ -97,7 +80,6 @@ class HyAIA:
             else:
                 min_values.loc[col] = ['N/A']
         
-        #Lista de valores con la media
         mean_values = pd.DataFrame(columns=['Mean_values'])
         for col in list(self.data.columns.values):
             if pd.api.types.is_numeric_dtype(self.data[col]):
@@ -108,7 +90,6 @@ class HyAIA:
             else:
                 mean_values.loc[col] = ['N/A']
         
-        #Lista de valores con su desviación estándar
         std_values = pd.DataFrame(columns=['Std_values'])
         for col in list(self.data.columns.values):
             if pd.api.types.is_numeric_dtype(self.data[col]):
@@ -123,12 +104,6 @@ class HyAIA:
                 .join(unique_values).join(is_categorical).join(categories)
                 .join(max_values).join(min_values).join(mean_values).join(std_values))
 
-    
-  #  def categoricos_limpieza(self):
-  #      for col in self.categoricos_columns:
-  #          self.data_categoricos[col] = self.data_categoricos[col].apply(remove_punctuation)
-            
-    # remover signos de puntuación
     @staticmethod
     def remove_punctuation(x):
         try:
@@ -137,6 +112,3 @@ class HyAIA:
             print(f'{x} no es una cadena de caracteres')
             pass
         return x
-
-    
-
